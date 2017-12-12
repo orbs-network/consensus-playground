6000b861ab3bfe5d0b68461aa2017bb2b5af3600
TalK <tal@orbs.network>
2017-12-12 02:13:26 +0200

    Another naive scenario

---
+++ src/simulation/connections/OnOffConstantDelay.ts
@@ -0,0 +1,37 @@
+import BaseConnection from "../BaseConnection";
+import BaseScenario from "../BaseScenario";
+import BaseNode from "../BaseNode";
+import MessageEvent from "../events/MessageEvent";
+import bind from "bind-decorator";
+
+export default class StableConstantDelay extends BaseConnection {
+  protected delayMs: number;
+  protected startsOn: boolean;
+  protected changesMs: number[];
+
+  constructor(scenario: BaseScenario, to: BaseNode, delayMs: number, startsOn: boolean, changesMs: number[]) {
+    super(scenario, to);
+    this.delayMs = delayMs;
+    this.startsOn = startsOn;
+    this.changesMs = changesMs;
+  }
+
+  @bind
+  isOnAtTimestamp(timestamp: number): boolean {
+    let currentOn = this.startsOn;
+    for (const change of this.changesMs) {
+      if (timestamp >= change) currentOn = !currentOn;
+      else break;
+    }
+    return currentOn;
+  }
+
+  @bind
+  send(message: any): void {
+    const timestamp = this.scenario.currentTimestamp + this.delayMs;
+    if (!this.isOnAtTimestamp(timestamp)) return;
+    const event = new MessageEvent(timestamp, this.to, message);
+    this.scenario.postEvent(event);
+  }
+
+}
