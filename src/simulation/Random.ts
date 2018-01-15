167052eb41c7b990ab852e71c3091a0615d43510
ronent <ronent@cs.huji.ac.il>
2018-01-15 20:25:24 +0200

    Adding random shuffle method

--- src/simulation/Random.ts
+++ src/simulation/Random.ts
@@ -18,4 +18,27 @@ export default class Random {
     return Math.round((max - min) * this.randomizer() + min);
   }
 
+  @bind
+  shuffle(array: number[]): number[] {
+    // based on https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let currentIndex = array.length;
+    let temporaryValue = -1;
+    let randomIndex = -1;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = this.nextIntegerInRange(0, (currentIndex - 1)); // inclusive
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
 }
