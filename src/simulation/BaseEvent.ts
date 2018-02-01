3f0f07628cc5b61b62ce84cac9556781442f1354
ronent <ronent@cs.huji.ac.il>
2018-02-01 18:52:53 +0200

    added uid to each event
    single unique id to be used for example by timer to identify timer expiry event when it happens

--- src/simulation/BaseEvent.ts
+++ src/simulation/BaseEvent.ts
@@ -4,14 +4,22 @@ import Endpoint from "./Endpoint";
 export default abstract class BaseEvent {
   public timestamp: number;
   public target: Endpoint;
+  public uid: number;
+  protected static numEvents = 0;
 
   constructor(timestamp: number, target: Endpoint) {
     this.timestamp = timestamp;
     this.target = target;
+    BaseEvent.numEvents++;
+    this.uid = BaseEvent.numEvents;
   }
 
   static comparator(a: BaseEvent, b: BaseEvent): number {
-    return a.timestamp - b.timestamp;
+    return a.timestamp - b.timestamp; // TODO maybe change to uid for deteministic effect for events with same timestamp?
+  }
+
+  isSameEvent(otherEvent: BaseEvent): boolean {
+    return (this.uid == otherEvent.uid);
   }
 
 }
