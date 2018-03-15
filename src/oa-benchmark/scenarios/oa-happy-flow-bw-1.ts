8f41943dec2cef56bd3fb2967fa01dd610d2eece
ronent <ronent@cs.huji.ac.il>
2018-03-15 14:56:38 +0200

    Issue #4  scenario for happy flow sim

--- src/oa-benchmark/scenarios/oa-happy-flow-bw-1.ts
+++ src/oa-benchmark/scenarios/oa-happy-flow-bw-1.ts
@@ -9,13 +9,13 @@ import OrbsBaseNode from "../../algorithms/oa-pbft/OrbsBaseNode";
 import OrbsScenario, { OrbsExpConfig } from "../../scenarios/oa-pbft/OrbsScenario";
 
 
-const NUM_NODES = [10];
-const COMMITTEE_SIZES = [7];
-const NUM_BYZ = [2]; // these won't actually be faulty, but used to determine f.
-const SHARING_THRESHOLDS = [5];
-const ETX_SIZES_BYTES = [250];
-const NUM_ETXS_PER_BLOCK =  [16384];
-const NODE_BW_BITS_SEC = [100000000];
+const TOTAL_NODE_FACTORS = [2, 1.5, 1];
+const COMMITTEE_SIZES = [4, 8, 16, 32, 64];
+// const NUM_BYZ = [2]; // these won't actually be faulty, but used to determine f.
+const SHARING_THRESHOLD_FACTORS = [1, 1.5, 2];
+const ETX_SIZES_BYTES = [250, 500];
+const NUM_ETXS_PER_BLOCK =  [128, 256, 512, 1024, 2048, 4096, 8192, 16384];
+const NODE_BW_BITS_SEC = [1000000000, 500000000, 100000000];
 const MAX_SIMULATION_TIMESTAMP_MS = 100000;
 const FAULTY_NODE_NAME = "HonestNode";
 const NETWORK_MODE = NetworkPropagationMode.Fastcast;
@@ -58,28 +58,25 @@ export default class Scenario extends BaseOrbsScenarioWithNode {
   static configs(): OrbsExpConfig[] {
     const oaConfigs: OrbsExpConfig[] = [];
     let c = 1;
-    for (const n of NUM_NODES) {
+    for (const nf of TOTAL_NODE_FACTORS) {
       for (const m of COMMITTEE_SIZES) {
-        for (const k of SHARING_THRESHOLDS) {
-          for (const b of NUM_BYZ) {
+        for (const k of SHARING_THRESHOLD_FACTORS) {
             for (const e of ETX_SIZES_BYTES) {
               for (const num_etxs of NUM_ETXS_PER_BLOCK) {
                   for (const bw_bits of NODE_BW_BITS_SEC) {
-                  const committeeSize = Math.min(m, n);
-                  const numByz = Math.min(committeeSize, b);
-                  const oaConfig: OrbsExpConfig = { name: `${c}`, nNodesToCreate: n, committeeSize: committeeSize, numByz: numByz, sharingThreshold: Math.min(k, n), faultyNodeName: FAULTY_NODE_NAME, networkConfiguration: OrbsScenario.getDefaultNetwork(n, e, num_etxs, bw_bits) };
+                  const numByz = Math.floor((m - 1) / 3);
+                  const n = Math.ceil(nf * m);
+                  const oaConfig: OrbsExpConfig = { name: `${c}`, nNodesToCreate: n, committeeSize: m, numByz: numByz, sharingThreshold: (numByz * k + 1), faultyNodeName: FAULTY_NODE_NAME, networkConfiguration: OrbsScenario.getDefaultNetwork(n, e, num_etxs, bw_bits) };
                   oaConfigs.push(oaConfig);
                   c++;
                 }
               }
             }
           }
-        }
       }
-    }
     return oaConfigs;
   }
-
+}
   @bind
   maxSimulationTimestampMs(): number {
     return MAX_SIMULATION_TIMESTAMP_MS;
